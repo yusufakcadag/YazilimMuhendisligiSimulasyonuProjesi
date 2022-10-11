7a61bdfeb9cc4995bb04a74066624a0428714766
Yusuf Akçadağ <akcadag.communication@outlook.com>
2022-10-12 01:35:42 +0300

    ekran hiyeraşisi bitti, ekran tasarımlarına devam ediliyor

---
+++ Proje/MyCar Rent Yazılım Sistemi/MyCarRent/ManagementBranch/FormCreateManagementBranch.cs
@@ -0,0 +1,10 @@
+namespace ManagementBranch
+{
+    public partial class FormCreateManagementBranch : Form
+    {
+        public FormCreateManagementBranch()
+        {
+            InitializeComponent();
+        }
+    }
+}
